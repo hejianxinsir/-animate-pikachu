fc2f2734d712cbd5b73d10627dac8771e83e0367
hejianxin <hejianxinsir@gmail.com>
2019-10-14 13:20:17 +0800

    调速无响应

---
+++ js/main.js
@@ -0,0 +1,207 @@
+!function(){
+	var css = `
+	*{
+		margin: 0; padding: 0;
+		box-sizing: border-box;
+	}
+	body{
+		background: #ffec00;
+		height: 100vh;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+	* > ::before{ box-sizing: border-box}
+	* > ::after{ box-sizing: border-box}
+	.wrapperPapa{
+		flex: 1;
+		height: 50%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.codeWrapper{
+		flex: 1;
+		height: 50%;
+		background: white;
+		overflow: hidden;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.codeWrapper > .code {
+		overflow: hidden;
+		height: 100%;
+		padding: 20px 30px;
+	}
+	.wrapper{
+		width: 100%;
+		height: 158px;
+		position: relative;
+	}
+	.nose{
+		position: absolute;
+		top: 23px;
+		left: 50%;
+		transform: translate(-50%);
+		width: 0;
+		border: 11px solid transparent;
+		border-radius: 50%;
+		border-top-color: black;
+	}
+
+	.eyes{
+		width: 43px;
+		height: 43px;
+		background: #2e2e2e;
+		border: 2px solid black;
+		border-radius: 50%;
+	}
+	.eyes.eyeLeft{
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translate(-50%);
+		margin-left: -100px;
+	}
+	.eyes.eyeRight{
+		position: absolute;
+		top: 0;
+		right: 50%;
+		transform: translate(50%);	
+		margin-right: -100px;
+	}
+	.eyebow{
+		width: 18px;
+		height: 18px;
+		background: white;
+		border-radius: 50%;
+
+		position: absolute;
+		top: 3px;
+		border: 2px solid black;
+	}
+	.eyebow.leftBow{
+		left: 50%;
+		transform: translate(-50%);
+		margin-left: -106px;
+	}
+	.eyebow.rightBow{
+		right: 50%;
+		transform: translate(50%);
+		margin-right: -93px;
+	}
+	.face{
+		width: 60px;
+		height: 60px;
+		border: 2px solid black;
+		border-radius: 50%;
+		background: #f00;
+
+		position: absolute;
+		top: 73px;
+	}
+	.face.faceLeft{
+		left: 50%;
+		margin-left: -163px;
+	}
+	.face.faceRight{
+		right: 50%;
+		transform: translate(50%);
+		margin-right:-133px;
+	}
+	.lip{
+		background: #ffec00;
+		width: 55px;
+		height: 22px;
+		border: 2px solid black;
+		position: absolute;
+		top: 45px;
+	}
+	.lip.topLeft{
+		border-bottom-left-radius: 45px 25px;
+		border-top: none;
+		border-right: none;
+		transform: rotate(-25deg);
+		right: 50%;	
+	}
+	.lip.topRight{
+		border-bottom-right-radius: 45px 25px;
+		border-top: none;
+		border-left: none;
+		transform: rotate(25deg);
+		left: 50%;
+	}
+	.tongueWrapper{
+		width: 128px;
+		height: 103px;
+		position: absolute;
+		bottom: 0;	
+		right: 50%;
+		overflow: hidden;
+		margin-right: -64px;
+		z-index:-1;
+	}
+	.tongue{
+		width: 92px;
+		height: 500px;
+		border: 2px solid black;
+		border-radius: 300px/1050px;	
+		position: absolute;
+		bottom: 0;
+		left: 50%;
+		transform: translate(-50%);
+		background: #a80000;
+		overflow: hidden;
+	}
+	.tongue::before{
+		position: absolute;
+		content: '';
+		bottom: -3px;
+		width: 70px;
+		height: 80px;
+		margin-left: 8px;
+		border-radius: 55px/64px;
+		background: #ff0051;
+	}
+
+	`
+	var duration = 10
+	writeCode('', css, ()=>{})
+	function writeCode(prefix,cssCode,fn){
+		var n = 0
+		var domCode = document.querySelector('.code')
+		var styleCode = document.querySelector('#styleCode')
+		var id = setInterval(function(){
+			n += 1
+			domCode.innerHTML = Prism.highlight(prefix + cssCode.slice(0,n), Prism.languages.css, 'css')
+			styleCode.innerHTML = prefix + cssCode.slice(0,n)
+			domCode.scrollTop = domCode.scrollHeight
+			if(n >= cssCode.length){
+				window.clearInterval(id)
+				fn && fn.call()
+			}
+		},duration)
+	}
+
+	$('.setSpeed').on('click','button',function(e){
+		var $button = $(e.currentTarget)
+		var speed = $button.attr('data-speed')
+		console.log(speed)
+		$button.addClass('active')
+			.siblings('.active').removeClass('active')
+		switch(speed){
+			case 'slow':
+				duration = 100
+				break
+			case 'normal':
+				duration = 50
+				break
+			case 'fast':
+				duration = 10
+				break
+		}	
+	})
+		
+}.call()
+
